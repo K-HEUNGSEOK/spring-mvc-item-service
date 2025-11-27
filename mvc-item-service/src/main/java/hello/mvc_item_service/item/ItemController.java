f843bc520513c69a240f4071a474b7dcbdd15d1b
김흥석 <eovnfjfpa@naver.com>
2025-11-27 18:14:42 +0900

    feat : editForm 구현 (item 수정 로직)

--- mvc-item-service/src/main/java/hello/mvc_item_service/item/ItemController.java
+++ mvc-item-service/src/main/java/hello/mvc_item_service/item/ItemController.java
@@ -1,12 +1,15 @@
 package hello.mvc_item_service.item;
 
+import jakarta.annotation.PostConstruct;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,5 +30,19 @@ public class ItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "items/item";
+    }
+
+    //뷰 페이지만 보여주는 것
+
+    @GetMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "/items/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String update(@PathVariable Long itemId , @ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
+        return "redirect:/items/{itemId}";
     }
 }
