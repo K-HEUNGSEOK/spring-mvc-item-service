b05d7f4ae1471f8ea3a1980c6f5f3d61e0ef1225
김흥석 <eovnfjfpa@naver.com>
2025-11-27 15:28:03 +0900

    feat : 프로젝트 추가

---
+++ mvc-item-service/src/test/java/hello/mvc_item_service/MvcItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mvc_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
