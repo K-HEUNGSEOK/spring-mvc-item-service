7ad49465a5316681c72c421bc4eea74c291b9ff9
김흥석 <eovnfjfpa@naver.com>
2025-11-27 22:17:40 +0900

    feat : init 클래스에 기본 member 추가

--- mvc-item-service/src/main/java/hello/mvc_item_service/Init.java
+++ mvc-item-service/src/main/java/hello/mvc_item_service/Init.java
@@ -2,6 +2,8 @@ package hello.mvc_item_service;
 
 import hello.mvc_item_service.item.model.Item;
 import hello.mvc_item_service.item.repository.ItemRepository;
+import hello.mvc_item_service.member.Member;
+import hello.mvc_item_service.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Component;
 public class Init {
 
     private final ItemRepository itemRepository;
-
+    private final MemberRepository memberRepository ;
     @PostConstruct
     public void init(){
+        Member member = new Member("김흥석", "test", "test!");
+        memberRepository.save(member);
         Item itemA = new Item("냉장고", 10000, 1);
         Item itemB = new Item("에어컨", 20000, 2);
         itemRepository.save(itemA);
