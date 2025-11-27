d4ab1f6fcd33c25efdcda86f2dcc86e9cb9f5ab8
김흥석 <eovnfjfpa@naver.com>
2025-11-27 16:30:04 +0900

    feat : memberRepository 기능 테스트

---
+++ mvc-item-service/src/test/java/hello/mvc_item_service/member/MemoryMemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package hello.mvc_item_service.member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Test
+    void memberSuccessTest(){
+        Member member = new Member("userA");
+        Member saveMember = memberRepository.save(member);
+        Optional<Member> findMember = memberRepository.findById(member.getMemberId());
+
+        assertThat(saveMember).isEqualTo(findMember.get());
+    }
+    @Test
+    void memberFailTest(){
+        assertThrows(NoSuchElementException.class, () -> memberRepository.findById(5L).get());
+    }
+
+}
