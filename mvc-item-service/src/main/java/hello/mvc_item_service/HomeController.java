243e3979c7658e7376001e33d35513529eb94749
김흥석 <eovnfjfpa@naver.com>
2025-11-27 21:34:10 +0900

    feat : home 화면 제작

---
+++ mvc-item-service/src/main/java/hello/mvc_item_service/HomeController.java
@@ -0,0 +1,12 @@
+package hello.mvc_item_service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
