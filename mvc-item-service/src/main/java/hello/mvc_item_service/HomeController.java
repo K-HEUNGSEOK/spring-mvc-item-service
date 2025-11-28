7bcd38a96791da111f7137c919ea7095d2b4c6de
김흥석 <eovnfjfpa@naver.com>
2025-11-28 11:41:16 +0900

    feat : 세션이 있는 사용자와 없는 사용자 따로 화면을 보여주는 로직 작성

--- mvc-item-service/src/main/java/hello/mvc_item_service/HomeController.java
+++ mvc-item-service/src/main/java/hello/mvc_item_service/HomeController.java
@@ -1,12 +1,20 @@
 package hello.mvc_item_service;
 
+import hello.mvc_item_service.member.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home(){
-        return "home";
+    public String home(@SessionAttribute(value = SessionLoginId.LOGIN_MEMBER, required = false) Member member,
+                       Model model) {
+        if (member == null) {
+            return "home";
+        }
+        model.addAttribute("member", member);
+        return "loginHome";
     }
 }
