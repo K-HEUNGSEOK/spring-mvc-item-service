33f3175c387581e98452286a5a57c9204a8b1127
김흥석 <eovnfjfpa@naver.com>
2025-11-28 11:51:31 +0900

    feat : 로그인 관련 인터셉터 클래스 생성

---
+++ mvc-item-service/src/main/java/hello/mvc_item_service/interceptor/MyInterceptor.java
@@ -0,0 +1,23 @@
+package hello.mvc_item_service.interceptor;
+
+import hello.mvc_item_service.util.SessionLoginId;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Slf4j
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String requestURI = request.getRequestURI();
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(SessionLoginId.LOGIN_MEMBER) == null){
+            log.info("미인증 사용자 로그인 시도 = {}", requestURI);
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+            return false;
+        }
+        return true;
+    }
+}
